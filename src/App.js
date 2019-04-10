965859fbcfb81563fb4dc30984c6b66b242e54ce
Lau <laurent.henriques@gmail.com>
2019-04-10 11:32:05 +0200

    US#000 Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Game from "./components/Game/Game";
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Game from './components/Game/Game';
 
 import './App.css';
 
@@ -9,12 +9,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/play" component={Game} />
-            </Switch>
-          </div>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/play" component={Game} />
+          </Switch>
+        </div>
       </div>
     );
   }
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import Map from './Map/Map';
 
 class Game extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-        }
-    }
+    };
+  }
 
-    render(){
-        return(
-            <div>
-                <button>Play Now !</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="gameContainer">
+        <button type="button">Play Now !</button>
+        <Map />
+      </div>
+    );
+  }
 }
 
 export default Game;
