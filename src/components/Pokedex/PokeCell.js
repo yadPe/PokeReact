6ce4c06363f9041ff702eaf35c5796eaad330ded
Benbrii <benoit.briastre@gmail.com>
2019-04-12 12:08:01 +0200

    Fixed Esling of PokeCell

--- src/components/Pokedex/PokeCell.js
+++ src/components/Pokedex/PokeCell.js
@@ -3,10 +3,10 @@ import './PokeCell.css';
 import sprites from './BBimg/sprites.png';
 
 const PokeCell = ({ pokeClass, handleOnClick }) => {
-    const { id, backgroundPosition } = pokeClass;
-    const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
-  
-    return <button onClick={() => handleOnClick(id)} style={style} className="poke-cell"></button>
+  const { id, backgroundPosition } = pokeClass;
+  const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
+
+  return <button type="button" onClick={() => handleOnClick(id)} style={style} className="poke-cell" />;
 };
 
 export default PokeCell;
