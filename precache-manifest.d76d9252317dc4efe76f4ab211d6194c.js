25ef0ab26889904406a185f384b6ba5a519322f8
YadPe <petitot.py@gmail.com>
2019-04-12 14:01:07 +0200

    Updates

---
+++ precache-manifest.d76d9252317dc4efe76f4ab211d6194c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b10254986b093477f21",
+    "url": "/lyon-0219-react-PokeReact/static/css/main.11821c71.chunk.css"
+  },
+  {
+    "revision": "8b10254986b093477f21",
+    "url": "/lyon-0219-react-PokeReact/static/js/main.adbcc306.chunk.js"
+  },
+  {
+    "revision": "7bc5cc447450c0193142",
+    "url": "/lyon-0219-react-PokeReact/static/js/runtime~main.13702a44.js"
+  },
+  {
+    "revision": "393091df5c9666711656",
+    "url": "/lyon-0219-react-PokeReact/static/js/2.7a8e1ae3.chunk.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/lyon-0219-react-PokeReact/static/media/sprites.6a398990.png"
+  },
+  {
+    "revision": "787f082d241d6a6cd266dc8317ef48a8",
+    "url": "/lyon-0219-react-PokeReact/static/media/001.787f082d.txt"
+  },
+  {
+    "revision": "96fd16fc4f453036ac92d0fa129a434f",
+    "url": "/lyon-0219-react-PokeReact/index.html"
+  }
+];
